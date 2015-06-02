66a32a7a43a09126e53eef996317bd891ba4a90f
周昊 <zhh032002@qq.com>
2015-06-02 11:55:45 +0800

    后端修改完成

--- app.py
+++ app.py
@@ -32,30 +32,16 @@ def pub(name):
     paper["venue"] = k["venue"] if "venue" in k else None
     paper["year"] = k["year"] if "year" in k else None
     paper["n_citation"] = k["n_citation"] if "n_citation" in k else None
+    # detail
+    detail = {}
+    detail["abstract"] = k["abstract"] if "abstract" in k else None
+    detail["abstract_zh"] = k["abstract_zh"] if "abstract_zh" in k else None
+    paper["detail"] = detail
     res.append(paper)
   jsonRes = json.dumps(res, indent=2, separators=(',', ': '))
   print jsonRes
   return jsonRes
 
-@app.route('/pub/<name>/detail')
-def pubDetail(name):
-  par = {
-    'query': name,
-    'size': 5,
-    'sort': 'relevance',
-    'offset': 0
-  }
-  raw = requests.get('http://api.aminer.org/api/search/pub', params = par)
-  dic = raw.json()
-  res = []
-  for k in dic["result"]:
-    paper = {}
-    paper["abstract"] = k["abstract"] if "abstract" in k else None
-    paper["abstract_zh"] = k["abstract_zh"] if "abstract_zh" in k else None
-    res.append(paper)
-  jsonRes = json.dumps(res, indent=2, separators=(',', ': '))
-  print jsonRes
-  return jsonRes
 
 @app.route('/people/<name>')
 def people(name):
@@ -68,7 +54,7 @@ def people(name):
   raw = requests.get('http://api.aminer.org/api/search/people', params = par)
   dic = raw.json()
   #多个搜索结果以列表形式返回，每一项是一个词典
-  #注意返回结果可能为None，因为Amine的api中可能没有这一项
+  #注意返回结果可能为None，因为Aminer的api中可能没有这一项
   res = []
   for k in dic["result"]:
     person = {}
@@ -82,29 +68,10 @@ def people(name):
     person["h_index"] = k["h_index"] if "h_index" in k else None
     person["n_pubs"] = k["n_pubs"] if "n_pubs" in k else None
     person["n_citation"] = k["n_citation"] if "n_citation" in k else None
-    res.append(person)
-    # person[""] = k[""] if "" in k else None
-  jsonRes = json.dumps(res, indent=2, separators=(',', ': '))
-  print jsonRes
-  return jsonRes
-
-@app.route('/people/<name>/detail')
-def peopleDetail(name):
-  par = {
-    'query': name,
-    'size': 5,
-    'sort': 'relevance',
-    'offset': 0
-  }
-  raw = requests.get('http://api.aminer.org/api/search/people', params = par)
-  dic = raw.json()
-  #多个搜索结果以列表形式返回，每一项是一个词典
-  #注意返回结果可能为None，因为Amine的api中可能没有这一项
-  res = []
-  for k in dic["result"]:
-    person = {}
-    person["id"] = k["id"] if "id" in k else None
-    person["contact"] = k["contact"] if "contact" in k else None
+    # detail
+    detail = {}
+    detail["id"] = k["id"] if "id" in k else None
+    detail["contact"] = k["contact"] if "contact" in k else None
     similar = []
     if "similarPersons" in k:
       for i in k["similarPersons"]:
@@ -113,15 +80,17 @@ def peopleDetail(name):
         j["name"] = i["name"] if "name" in i else None
         j["image"] = i["img"] if "img" in i else None
         similar.append(j)
-      person["similarPersons"] = similar
+      detail["similarPersons"] = similar
     else:
-      person["similarPersons"] = None
+      detail["similarPersons"] = None
+    person["detail"] = detail
     res.append(person)
     # person[""] = k[""] if "" in k else None
   jsonRes = json.dumps(res, indent=2, separators=(',', ': '))
   print jsonRes
   return jsonRes
-  
+
+
 @app.route('/entity/<name>')
 def entity(name):
   raw = requests.get('http://api.aminer.org/api/search/entity/' + name)
@@ -130,29 +99,22 @@ def entity(name):
   for k in dic:
     entity = {}
     entity["title"] = k["title"] if "title" in k else None
-    entity["image"] = k["image"][0] if "image" in k else None
+    entity["image"] = k["image"][0] if "image" in k and len(k["image"]) > 0 else None
+    #detail
+    detail = {}
+    detail["url"] = k["url"] if "url" in k else None
+    detail["image"] = k["image"] if "image" in k else None
+    detail["abstract"] = k["abstract"] if "abstract" in k else None
+    detail["super_topic"] = k["super_topic"] if "super_topic" in k else None
+    detail["related_item"] = k["related_item"] if "related_item" in k else None
+    detail["type"] = k["type"] if "type" in k else None
+    entity["detail"] = detail
     res.append(entity)
   jsonRes = json.dumps(res, indent=2, separators=(',', ': '))
   print jsonRes
   return jsonRes
 
-@app.route('/entity/<name>/detail')
-def entityDetail(name):
-  raw = requests.get('http://api.aminer.org/api/search/entity/' + name)
-  dic = raw.json()
-  res = []
-  for k in dic:
-    entity = {}
-    entity["url"] = k["url"] if "url" in k else None
-    entity["image"] = k["image"] if "image" in k else None
-    entity["abstract"] = k["abstract"] if "abstract" in k else None
-    entity["super_topic"] = k["super_topic"] if "super_topic" in k else None
-    entity["related_item"] = k["related_item"] if "related_item" in k else None
-    entity["type"] = k["type"] if "type" in k else None
-    res.append(entity)
-  jsonRes = json.dumps(res, indent=2, separators=(',', ': '))
-  print jsonRes
-  return jsonRes
+
 
 if __name__ == '__main__':
   app.run(debug=True)
