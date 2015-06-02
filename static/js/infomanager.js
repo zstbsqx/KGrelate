31cb93f113caa17e79e75a249714085a4111ff08
unknown <zstbcj@gmail.com>
2015-06-03 00:50:50 +0800

    add people detail

--- static/js/infomanager.js
+++ static/js/infomanager.js
@@ -14,7 +14,12 @@ var InfoManager = function (id) {
     entity: false
   };
 };
-  
+
+InfoManager.load = function(name, options) {
+  this.loadPubs();
+  this.loadPeople();
+};
+
 // $.get status: ("success", "notmodified", "nocontent", "error", "timeout", "abort", or "parsererror")
 InfoManager.prototype.loadPubs = function (name, options) { //Arg options are for further dev
   console.log('loadpubs: ' + name);
@@ -79,15 +84,54 @@ var renderPub = function (dom, data) {
 
 var renderPerson = function(dom, data) {
   var id = 'persondetail_' + data.id;
+  var contactList = '<ul class="vcard">';
+  if(data.detail.contact) {
+    var hasContent = false;
+    //TODO: filter contents, move bio outside, add captions
+    for(var i in data.detail.contact) {
+      if(data.detail.contact[i]) {
+        contactList += '<li class="' + i + '">' + data.detail.contact[i] + '</li>';
+        hasContent = true;
+      }
+    }
+    if(!hasContent) {
+      contactList += '<li>No contact info</li>';
+    }
+  } else {
+    contactList += '<li>No contact info</li>';
+  }
+  contactList += '</ul>';
+  var similarPersonList = '<ul class="small-block-grid-6">';
+  if(data.detail.similarPersons) {
+    var hasSimilar = false;
+    for(var i in data.detail.similarPersons) {
+      var person = data.detail.similarPersons[i];
+      similarPersonList += '<li class="personthumbnail">'
+        + '<div><img class="th" src="' + (person.image || 'static/img/default.jpg') + '" alt="' + person.name + '"></div>'
+        + '<div>' + person.name + '</div>'
+        + '</li>';
+      hasSimilar = true;
+    }
+  }
+  //TODO: if !hasContent then ?
+  similarPersonList += '</ul>';
   var summary = $('<div class="summary row" />'),
       detail = $('<ul class="detail accordion" data-accordion>'
                  + '<li class="accordion-navigation">'
                  + '<a href="#' + id + '">click for more details</a>'
-                 + '<div id="' + id + '" class="content panel">Loading...</div>'
-                 + '</li></ul>');
+                 + '<div id="' + id + '" class="content panel">' 
+                 + '<div class="contact">'
+                 + '<div>contact:</div>'
+                 + contactList
+                 + '</div>'
+                 + '<div class="similarPersons">'
+                 + '<div>similar persons:</div>'
+                 + similarPersonList
+                 + '</div>'
+                 + '</div></li></ul>');
   //TODO: change 'loading...' to detail content
   summary.append('<div class="photo small-2 columns">'
-                 + '<img class="th" src="' + data.image || '/static/image/default.jpg' + '">'
+                 + '<img class="th" src="' + (data.image || 'static/img/default.jpg') + '">'
                  + '</div>');
   var info = $('<div class="small-10 columns" />');
   var name = data.name;
@@ -100,9 +144,9 @@ var renderPerson = function(dom, data) {
     org += '(' + data.org_zh + ')';
   }
   info.append('<div class="org">' + org + '</div>')
-//  if(data.contact.position) {
-//    info.append('<div class="position">' + data.contact.position + '</div>');
-//  }
+  //  if(data.contact.position) {
+  //    info.append('<div class="position">' + data.contact.position + '</div>');
+  //  }
   var pubinfo = $('<div class="pubinfo" />');
   pubinfo.append('pubinfo:');
   pubinfo.append('<span class="label">h_index : ' + data.h_index + '</span>');
@@ -129,11 +173,13 @@ var renderPerson = function(dom, data) {
 var renderLayout = function(dom, type, data) {
   console.log('renderlayout:\ntype: ' + type + '\ntarget dom: ' + dom);
   var id = 'relate_' + type;
-  var main = dom.children(id);
+  var main = dom.children('#' + id);
   if(main.length === 0) {
     main = $('<div id="' + id+ '" />');
     dom.prepend(main);
     console.log('new main')
+  } else {
+    main.empty();
   }
   main.append('<div>Relate ' + type + ':</div>');
   var mainlist = $('<ul class="no-bullet" />');
